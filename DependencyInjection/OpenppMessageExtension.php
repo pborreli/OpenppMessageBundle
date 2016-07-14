ae466b8a5dd90e13f27a55c277842464b87b588d
Taniguchi Yoshihiro <taniguchi@webware.co.jp>
2016-07-14 19:58:22 +0900

    fix

--- DependencyInjection/OpenppMessageExtension.php
+++ DependencyInjection/OpenppMessageExtension.php
@@ -1,6 +1,7 @@
 <?php
 namespace Openpp\MessageBundle;
 
+use Sonata\EasyExtendsBundle\Mapper\DoctrineCollector;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
@@ -11,5 +12,27 @@ class OpenppMessageExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
+        $this->registerDoctrineMapping($config);
+    }
+
+    /**
+     * @param array $config
+     */
+    protected function registerDoctrineMapping(array $config)
+    {
+        $collector = DoctrineCollector::getInstance();
+
+        // One-To-Many Bidirectional for Application and User
+        $collector->addAssociation($config['class']['message'], 'mapOneToMany', array(
+            'fieldName'     => 'users',
+            'targetEntity'  => $config['class']['user'],
+            'cascade'       => array(
+                'remove',
+                'persist',
+            ),
+            'mappedBy'      => 'application',
+            'orphanRemoval' => false,
+        ));
     }
 }
